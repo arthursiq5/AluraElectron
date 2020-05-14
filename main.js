1e2e82832c705194db41edd0121bec1b960c59cc
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-14 19:35:24 -0300

    criada primeira tela e carregados dados do site da Alura

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Olá, mundo!");
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+    console.log('Aplicação iniciada');
+
+    let mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+
+    mainWindow.loadURL("https://www.alura.com.br");
+});
