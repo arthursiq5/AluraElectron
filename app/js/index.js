2205dd5d644dc715383f61d97ab004d86b5055ab
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-14 22:46:17 -0300

    inserindo tela "about"

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,7 @@
-console.log('arquivo importado');
+const { ipcRenderer } = require('electron');
+
+let linkSobre = document.getElementById('link-sobre');
+
+linkSobre.addEventListener('click', () => {
+    ipcRenderer.send('abrir-janela-sobre');
+});
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 app.on('ready', () => {
     console.log('Aplicação iniciada');
@@ -13,4 +13,13 @@ app.on('ready', () => {
 
 app.on('window-all-closed', () => {
     app.quit();
+});
+
+ipcMain.on('abrir-janela-sobre', () => {
+    let sobreWindow = new BrowserWindow({
+        width: 300,
+        height: 200
+    });
+
+    sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`);
 });
