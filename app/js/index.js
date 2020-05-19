9601572db6e0f68e2f1219ed599e04b2fdbf07f4
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-18 22:55:15 -0300

    trocando ícone do botão ao clicar

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,15 @@
 const { ipcRenderer } = require('electron');
 
 let linkSobre = document.getElementById('link-sobre');
+let botaoPlay = document.querySelector('.botao-play');
+
+let imgs = ['./img/play-button.svg', './img/stop-button.svg']
 
 linkSobre.addEventListener('click', () => {
     ipcRenderer.send('abrir-janela-sobre');
 });
+
+botaoPlay.addEventListener('click', () => {
+    imgs = imgs.reverse();
+    botaoPlay.src = imgs[0];
+}) 
