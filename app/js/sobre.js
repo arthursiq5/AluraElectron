2a396ef7c0bfb341ed1dcb1c2a3ca7d1a9a8a94c
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-18 21:35:07 -0300

    Atualizando dinamicamente versão do Electron na tela "sobre"

--- app/js/sobre.js
+++ app/js/sobre.js
@@ -1,7 +1,18 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, shell } = require('electron');
+const process = require('process');
 
 let linkFechar = document.getElementById('link-sobre');
+let linkGithub = document.getElementById('link-github');
+let versaoElectron = document.getElementById('versao-electron');
 
 linkFechar.addEventListener('click', () => {
     ipcRenderer.send('fechar-janela-sobre');
 });
+
+linkGithub.addEventListener('click', () => {
+    shell.openExternal('https://github.com/arthursiq5')
+});
+
+window.onload = () => {
+    versaoElectron.textContent = process.versions.electron;
+}
