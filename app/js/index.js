b5e7545da0a93fa00aeb24991c30d4b0645056dc
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-19 23:16:52 -0300

    utilizando corretamente funções do módulo

--- app/js/index.js
+++ app/js/index.js
@@ -6,11 +6,14 @@ let botaoPlay = document.querySelector('.botao-play');
 
 let imgs = ['./img/play-button.svg', './img/stop-button.svg']
 
+let tempo = document.querySelector('.tempo');
+
 linkSobre.addEventListener('click', () => {
     ipcRenderer.send('abrir-janela-sobre');
 });
 
 botaoPlay.addEventListener('click', () => {
     imgs = imgs.reverse();
+    timer.iniciar(tempo);
     botaoPlay.src = imgs[0];
 }) 
