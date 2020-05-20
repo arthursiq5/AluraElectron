8d70ce9758ee49aa3739916e3c2669cca00c12d7
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-19 23:09:58 -0300

    passando tempo em segundos para variável

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,8 +1,11 @@
 const moment = require('moment');
 
+let segundos;
+
 module.exports = {
     iniciar(el) {
         let tempo = moment.duration(el.textContent);
+        segundos = tempo.asSeconds();
         setInterval(function () {
 
         }, 1000);
