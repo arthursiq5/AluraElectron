c65afb849c9584da713d6248a2c0bdaf1b541e4e
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-20 01:27:36 -0300

    realizadas correções para entrar em conformidade com o curso

--- data.js
+++ data.js
@@ -13,41 +13,37 @@ module.exports = {
                 });
         }
     },
-    
     adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado) {
         let dados = {
             ultimoEstudo: new Date().toString(),
             tempo: tempoEstudado
         }
-        jsonfile.writeFile(arquivoDoCurso, dados)
+
+        jsonfile.writeFile(arquivoDoCurso, dados, { spaces: 2 })
             .then(() => {
                 console.log('Tempo salvo com sucesso');
             }).catch((err) => {
                 console.log(err);
             })
     },
-
     criaArquivoDeCurso(nomeArquivo, conteudoArquivo) {
-        jsonfile
+        return jsonfile
             .writeFile(nomeArquivo, conteudoArquivo)
             .then(() => {
-                console.log('Arquivo Criado');
+                console.log('Arquivo Criado')
             }).catch((err) => {
                 console.log(err);
             });
     },
-
     pegaDados(curso) {
         let arquivoDoCurso = __dirname + '/data/' + curso + '.json';
         return jsonfile.readFile(arquivoDoCurso);
     },
-
     pegaNomeDosCursos() {
         let arquivos = fs.readdirSync(__dirname + '/data/');
         let cursos = arquivos.map((arquivo) => {
             return arquivo.substr(0, arquivo.lastIndexOf('.'));
         });
-
         return cursos;
     }
 }
